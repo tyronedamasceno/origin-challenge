a7d0fbf469ae1d98a9f5296636a6d798e2f63ca9
Tyrone Damasceno <tyronedamasceno@gmail.com>
2020-01-04 10:51:27 -0300

    Add Swagger documentation

--- insurance_risk/api/views.py
+++ insurance_risk/api/views.py
@@ -1,11 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from drf_yasg.utils import swagger_auto_schema
+
 from api.core import calculate_risk_profile
 from api.serializers import UserInputSerializer, RiskProfileSerializer
 
 
 class RiskProfileView(APIView):
+    @swagger_auto_schema(request_body=UserInputSerializer,
+                         responses={'200': RiskProfileSerializer})
     def post(self, request):
         serializer = UserInputSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
